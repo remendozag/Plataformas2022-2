450d154c35cc96c3a727bf071d70a0a033760ad9
richardmendozag <99446620+richardmendozag@users.noreply.github.com>
2022-02-16 10:40:57 -0500

    Version 1.0
    Versión 1.0

---
+++ AppCondicionales/AppCondicionales/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace  SentenciaIF
+{
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            /*Se ingresa por teclado un número positivo 
+             * de uno o dos dígitos (1..99) mostrar un mensaje
+             * indicando si el número tiene uno o dos dígitos
+             * (Tener en cuenta que condición debe cumplirse 
+             * para tener dos dígitos, un número entero)
+             */
+            int numero;
+            Console.WriteLine("Digite número (1-99):");
+            numero = int.Parse(Console.ReadLine());
+            if (numero < 10)
+            {
+                Console.WriteLine("El numero tiene un dígito");
+            }
+            else if (numero <= 99)
+            {
+                Console.WriteLine("El número tiene dos dígitos");
+            }
+            else 
+            {
+                Console.WriteLine("El número tiene más de dos dígitos");
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ AppOperadores/AppOperadores/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Operaciones {
+    class Program {
+        static void Main(string[] args) 
+        {
+            int num1, num2, num3, num4, suma;
+            decimal promedio, decimales;
+            Console.WriteLine("Programa Suma y promedio");
+            Console.WriteLine("Digite número 1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite número 2:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite número 3:");
+            num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite número 4:");
+            num4 = int.Parse(Console.ReadLine());
+            suma = num1 + num2 + num3 + num4;
+            Console.WriteLine("La suma es:"+suma);
+            promedio = suma / 4;
+            decimales= Math.Round(promedio, 2);
+            Console.WriteLine("El promedio es:" + decimales);
+            Console.ReadKey();
+        }
+
+    }
+}
